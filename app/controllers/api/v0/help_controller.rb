80c685bd9ffef3c02cefd1876e10aea8f5bdbf04
JonErikDSuero <jonerikdsuero@gmail.com>
2014-11-10 14:40:41 -0500

    hello world

---
+++ app/controllers/api/v0/help_controller.rb
@@ -0,0 +1,7 @@
+class Api::V0::HelpController < ApplicationController
+
+  def index
+    
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+
+  get '/auth/:provider/callback', to: 'sessions#create'
+
+  scope module: :api do
+
+    namespace :v0 do
+
+      resources :help
+
+    end
+
+  end
+end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +67,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
