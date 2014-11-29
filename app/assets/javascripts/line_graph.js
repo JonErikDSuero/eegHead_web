15fcc8d1b4a450e78d80b8acd7343394bbe7b090
JonErikDSuero <jonerikdsuero@gmail.com>
2014-11-29 14:10:30 -0500

    multiple wave graphs

--- app/assets/javascripts/line_graph.js
+++ app/assets/javascripts/line_graph.js
@@ -2,16 +2,21 @@
 var m = [80, 80, 80, 80]; // margins
 var w = 1250 - m[1] - m[3]; // width
 var h = 500 - m[0] - m[2]; // height
+var graph = {}
 
 // define graph and x-y axes
-var graph = d3.select("#graph").append("svg:svg")
-.attr("width", w + m[1] + m[3])
-.attr("height", h + m[0] + m[2])
-.append("svg:g")
-.attr("transform", "translate(" + m[3] + "," + m[0] + ")");
+$('.aGraph').each( function(a_graph){
+  var wave_type = $(this).attr("wave_type");
+  graph[wave_type] = d3.select(".aGraph[wave_type='"+wave_type+"']").append("svg:svg")
+  .attr("width", w + m[1] + m[3])
+  .attr("height", h + m[0] + m[2])
+  .append("svg:g")
+  .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
 
+  updateLineChart(wave_type);
+});
 
-function drawLineChart(data){
+function drawLineChart(data, wave_type){
   var x = d3.time.scale().domain([new Date(data.date_min), new Date(data.date_max)]).range([0, w]);
   var y = d3.scale.linear().domain([data.value_min, data.value_max]).range([h, 0]);
 
@@ -27,24 +32,22 @@ function drawLineChart(data){
   var yAxis = d3.svg.axis().scale(y).ticks(4).orient("left");
 
   // Draw
-  graph.append("svg:g")
+  graph[wave_type].append("svg:g")
   .attr("class", "x axis")
   .attr("transform", "translate(0," + h + ")");
-  graph.selectAll("g .x.axis").call(xAxis);
+  graph[wave_type].selectAll("g .x.axis").call(xAxis);
 
-  graph.append("svg:g")
+  graph[wave_type].append("svg:g")
   .attr("class", "y axis")
   .attr("transform", "translate(-25,0)");
-  graph.selectAll("g .y.axis").call(yAxis);
+  graph[wave_type].selectAll("g .y.axis").call(yAxis);
 
-  graph.append("svg:path").attr("d", line(data.points));
+  graph[wave_type].append("svg:path").attr("d", line(data.points));
 }
 
 function updateLineChart(wave_type){
   $.post( "/v1/waves/graph_points", { wave_type: wave_type}, function( data ) {
-    drawLineChart(data);
+    drawLineChart(data, wave_type);
   }, "json");
 }
 
-updateLineChart("wave0");
-//updateLineChart("wave1");
--- app/models/wave.rb
+++ app/models/wave.rb
@@ -2,18 +2,18 @@ class Wave < ActiveRecord::Base
 
   def self.categorize(waves_values)
     waves = {
-      wave0: waves_values[0],
-      wave1: waves_values[1],
-      wave2: waves_values[2],
-      wave3: waves_values[3],
-      wave4: waves_values[4],
-      wave5: waves_values[5],
-      wave6: waves_values[6],
-      wave7: waves_values[7],
-      attention: waves_values[8],
-      meditation: waves_values[9],
-      blink: waves_values[10],
-      quality: waves_values[11]
+      quality: waves_values[0],
+      wave0: waves_values[1],
+      wave1: waves_values[2],
+      wave2: waves_values[3],
+      wave3: waves_values[4],
+      wave4: waves_values[5],
+      wave5: waves_values[6],
+      wave6: waves_values[7],
+      wave7: waves_values[8],
+      attention: waves_values[9],
+      meditation: waves_values[10],
+      blink: waves_values[11],
     }
     return waves
   end
