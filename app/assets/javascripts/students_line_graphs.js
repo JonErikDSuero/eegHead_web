de890471ff4b99170f42d7ca8b617ab64e721774
JonErikDSuero <jonerikdsuero@gmail.com>
2015-04-01 07:47:20 -0400

    Average

--- app/assets/javascripts/students_line_graphs.js
+++ app/assets/javascripts/students_line_graphs.js
@@ -3,18 +3,32 @@ var m = [50, 50, 50, 117]; // margins [top, right, bottom, left]
 var w = 854; // width
 var h = 480;// height
 var graphStudents = {}
+var numGraphStudents = 0;
+
+var graphAverage;
+
+var dataAll = {};
 
 // initialize line_charts
-$('#studentsModal .aGraph').each( function(a_graph){
+$('.aGraph.student').each( function(a_graph){
   var studentCode = $(this).attr("student_code");
-  graphStudents[studentCode] = d3.select("#studentsModal .aGraph[student_code='"+studentCode+"']").append("svg:svg")
+  graphStudents[studentCode] = d3.select(".aGraph.student[student_code='"+studentCode+"']").append("svg:svg")
   .attr("width", w + m[1] + m[3])
   .attr("height", h + m[0] + m[2])
   .append("svg:g")
   .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
+  numGraphStudents++;
 });
 
-function updateStudentLineChart(data, studentCode){
+
+graphAverage = d3.select(".aGraph.average").append("svg:svg")
+.attr("width", w + m[1] + m[3])
+.attr("height", h + m[0] + m[2])
+.append("svg:g")
+.attr("transform", "translate(" + m[3] + "," + m[0] + ")");
+
+
+function updateStudentLineChart(data, graphToUpdate){
   var video_duration = Math.trunc(youtube_player.getDuration());
 
   if (data.y_min == data.y_max) { // fix if min and max are the same
@@ -41,32 +55,73 @@ function updateStudentLineChart(data, studentCode){
   var yAxis = d3.svg.axis().scale(y).orient("left").ticks(4);
 
   // Draw
-  graphStudents[studentCode].append("svg:g")
+  graphToUpdate.append("svg:g")
   .attr("class", "x axis")
   .attr("transform", "translate(0," + h + ")");
-  graphStudents[studentCode].selectAll("g .x.axis").call(xAxis);
+  graphToUpdate.selectAll("g .x.axis").call(xAxis);
 
-  graphStudents[studentCode].append("svg:g")
+  graphToUpdate.append("svg:g")
   .attr("class", "y axis")
   .attr("transform", "translate(-25,0)");
-  graphStudents[studentCode].selectAll("g .y.axis").call(yAxis);
+  graphToUpdate.selectAll("g .y.axis").call(yAxis);
 
-  graphStudents[studentCode].selectAll("path").remove();
-  graphStudents[studentCode].append("svg:path")
+  graphToUpdate.selectAll("path").remove();
+  graphToUpdate.append("svg:path")
   .attr('d', line(data.points));
 }
 
 //Video Sessions Buttons
 $('body').on('click', '.students .session', function() {
-  $('#studentsModal .aGraph').each( function(a_graph){
+
+  dataAll.raw = {};
+  var counter = 0;
+
+  $('.aGraph.student').each( function(a_graph){
     var studentCode = $(this).attr("student_code");
     params = {
       video_session_code: studentCode
     }
     $.post('/v1/video_sessions/graph_points', params, function(data){
-      updateStudentLineChart(data["attention"], studentCode);
+      addToDataAll(dataAll, data["attention"]);
+      updateStudentLineChart(data["attention"], graphStudents[studentCode]);
       $('#studentsModal #waves_graphs').show()
+
+      counter++;
+      if (counter==numGraphStudents){
+        averageDataAll();
+        updateStudentLineChart(dataAll, graphAverage);
+      }
+
     });
   });
 });
+
+
+function addToDataAll(dataAll, src){
+
+  if (isNaN(dataAll.y_min) || (dataAll.y_min > src.y_min)){
+    dataAll.y_min = src.y_min
+  }
+
+  if (isNaN(dataAll.y_max) || (dataAll.y_max < src.y_max)){
+    dataAll.y_max = src.y_max
+  }
+
+  var srclen = src.points.length;
+  for (var i = 0; i < srclen; i++) {
+    if (dataAll.raw[src.points[i][0]] == undefined) {
+      dataAll.raw[src.points[i][0]] = 0;
+    }
+    dataAll.raw[src.points[i][0]] += (src.points[i][1])/numGraphStudents;
+  }
+
+  1==1;
+}
+
+function averageDataAll(){
+  dataAll.points = [];
+  $.each( dataAll.raw, function(index, value){
+    dataAll.points.push([parseInt(index), value]);
+  });
+}
 
